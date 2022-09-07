6453eef39d966511f1cab7732eb5ac706698583e
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-09-07 12:50:25 +0300

    added new guest

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -6,6 +6,8 @@ import Header from "./components/navigation/Header";
 import GuestProf from "./components/guest/GuestProf";
 import Navbar from "./components/navigation/Navbar";
 import GuestNav from "./components/navigation/GuestNav";
+import GuestLogin from "./components/login/GuestLogin";
+
 
 
 
@@ -27,12 +29,20 @@ const App = () => {
       .then(fetchedRestaurants => setRestaurants(fetchedRestaurants))
   }, [])
 
+  const [currentGuestId, setCurrentGuestId] = useState("")
+  const [loggedInGuest, setLoggedInGuest] = useState({})
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const addNewGuest = (newGuest) => {
+    setGuests([...guests, newGuest])
+  }
+
   return (
     <BrowserRouter>
     <Header />
-     <GuestNav /> 
-     <Navbar />
-    <Routes>
+    {isLoggedIn ?<GuestNav /> :<Navbar />}
+     <Routes>
+        <Route exact path="/" element={<GuestLogin guests={guests} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} currentGuestId={currentGuestId} setCurrentGuestId={setCurrentGuestId} loggedInGuest={loggedInGuest} setLoggedInGuest={setLoggedInGuest} onAddNewGuest={addNewGuest} />} />
         <Route exact path="/restaurants" element={<RestaurantList restaurants={restaurants} />} />
         <Route exact path="/profile" element={<GuestProf />} />
         <Route exact path="/reservations" element={<ReservationList/>} />
