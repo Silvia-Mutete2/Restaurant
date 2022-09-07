4b4ed8c1d145204fe1d403a0d721f9b55d670e00
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-09-07 11:33:51 +0300

    added react router

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -1,5 +1,6 @@
 import RestaurantList from "./components/restaurant/RestaurantList";
 import ReservationList from "./components/reservations/ReservationList";
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 const App = () => {
 
