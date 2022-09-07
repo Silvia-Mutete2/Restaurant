dc139f762fe37ebba95656b85b1236c109a50aeb
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-09-07 11:42:54 +0300

    restaurant navbar

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -1,6 +1,8 @@
 import RestaurantList from "./components/restaurant/RestaurantList";
 import ReservationList from "./components/reservations/ReservationList";
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Header from "./components/navigation/Header";
+
 
 const App = () => {
 
