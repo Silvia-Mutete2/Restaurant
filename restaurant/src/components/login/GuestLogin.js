af70bc14c9a511f0388552e6af25f4f23c8c31e2
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-09-07 12:41:30 +0300

    set guest login

--- restaurant/src/components/login/GuestLogin.js
+++ restaurant/src/components/login/GuestLogin.js
@@ -1,15 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import LoginHome from './LoginHome'
+import LoggedHome from './LoggedHome'
 
 
-
-
-const GuestLogin = () => {
+const GuestLogin = ({ guests, isLoggedIn, setIsLoggedIn, currentGuestId, setCurrentGuestId, loggedInGuest, setLoggedInGuest}) => {
 
     return (
-
         <div>
-            
+        {!!isLoggedIn ? <LoggedHome loggedInGuest={loggedInGuest}/> : <LoginHome guests={guests} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} currentGuestId={currentGuestId} setCurrentGuestId={setCurrentGuestId} loggedInGuest={loggedInGuest} setLoggedInGuest={setLoggedInGuest}/>}
+
             <Outlet />
             
         </div >
--- restaurant/src/components/login/LoggedHome.js
+++ restaurant/src/components/login/LoggedHome.js
@@ -8,3 +8,4 @@ const LoggedHome = ({loggedInGuest}) => {
   }
   
   export default LoggedHome
+  
