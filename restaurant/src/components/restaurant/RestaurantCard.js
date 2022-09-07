7cba71f937960e7e30cf130e36923dd3c8d766fd
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-09-07 10:40:59 +0300

    restaurant card

---
+++ restaurant/src/components/restaurant/RestaurantCard.js
@@ -0,0 +1,15 @@
+import {Link} from "react-router-dom"
+
+const RestaurantCard = ({restaurant, isLoggedIn}) => {
+ 
+  return (
+    <div>
+      <img src={restaurant.image_url} alt="restaurant image" width="200" height="200"/>
+      <h3>{restaurant.restaurant_name}</h3>
+      <p>{restaurant.cuisine_type}</p>
+      {isLoggedIn ? <Link to={"/restaurants/" + restaurant.id}>Make a Reservation</Link> : <p><em>Please log in to make a reservation!</em></p>}
+    </div>
+     )
+}
+
+export default RestaurantCard
