ac69f6e8e1646d625184f52047d558fc5d771d8f
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-09-07 12:36:11 +0300

    created a Logged In page

---
+++ restaurant/src/components/login/LoggedHome.js
@@ -0,0 +1,10 @@
+const LoggedHome = ({loggedInGuest}) => {
+    return (
+      <div>
+        <h2>Welcome, {loggedInGuest.name}</h2>
+        <p>Use the tabs above to find restaurants, make reservations, and view your reservations.</p>
+      </div>
+    )
+  }
+  
+  export default LoggedHome
