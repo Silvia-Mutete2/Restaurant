a1deee4f06225207dfd493247216a9390b5f36c6
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-09-07 11:57:51 +0300

    guests profile

---
+++ restaurant/src/components/guest/GuestProf.js
@@ -0,0 +1,27 @@
+import ReservationCard from "../reservations/ReservationCard"
+import { Link, useNavigate } from "react-router-dom"
+
+
+const GuestProf= ({loggedInGuest, setLoggedInGuest, setIsLoggedIn}) => {
+
+  let navigate = useNavigate()
+
+  const logOutGuest = () => {
+    setLoggedInGuest({})
+    setIsLoggedIn(false)
+    navigate("/")
+  }
+ 
+  return (
+    <div>
+      <h3>{loggedInGuest.name}'s Profile</h3>
+
+      <p>username: {loggedInGuest.username}</p>
+      <Link to={"/reservations"}>View and edit my reservations</Link>
+      <button onClick={logOutGuest}>Log Out</button>
+    </div>
+    
+  )
+}
+
+export default GuestProf
