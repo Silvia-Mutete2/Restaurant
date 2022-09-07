711729a1ca15e975db6af2ac26dc91b01b67c97a
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-09-07 09:06:21 +0300

    reservation list

---
+++ restaurant/src/components/reservations/ReservationList.js
@@ -0,0 +1,23 @@
+import ReservationCard from './ReservationCard'
+
+const ReservationList = ({ loggedInGuest, onDeleteReservation, onUpdateReservation }) => {
+
+  console.log("loggedInGuest in reservation list", loggedInGuest)
+
+  if (loggedInGuest.reservations.length > 0) {
+    return (
+      <div>
+        <h3>{loggedInGuest.name}'s Reservations</h3>
+        {loggedInGuest.reservations.map((reservation) => <ReservationCard loggedInGuest={loggedInGuest} reservation={reservation} key={reservation.id} onDeleteReservation={onDeleteReservation} onUpdateReservation={onUpdateReservation}/>)}
+      </div>
+    )
+  } else {
+    return (
+      <h3><em>You have no reservations</em></h3>
+    )
+  }
+
+  
+}
+
+export default ReservationList
