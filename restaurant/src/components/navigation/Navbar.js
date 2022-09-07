71d71d6a56707bb89ff84d3d9ea26c6d817acb90
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-09-07 11:46:21 +0300

    created navbar

---
+++ restaurant/src/components/navigation/Navbar.js
@@ -0,0 +1,31 @@
+import {NavLink} from 'react-router-dom'
+
+const style = {
+    width: "60%",
+    margin: "5% 0 1%",
+    padding: "1em",
+    textDecoration: "none",
+    color: "black",
+    verticalAlign: "center"
+}
+const activeStyle = {
+    width: "60%",
+    margin: "5% 0 1%",
+    padding: "1em",
+    textDecoration: "none",
+    color: "black",
+    fontWeight: "bold",
+    verticalAlign: "center",
+    textDecoration: "underline",
+  };
+
+const Navbar = () => {
+    return (
+        <div>
+            <NavLink style={({ isActive }) => isActive ? activeStyle : style } to="/">Home</NavLink>
+            <NavLink style={({ isActive }) => isActive ? activeStyle : style} to="/restaurants">Restaurants</NavLink>
+        </div>
+    )
+}
+
+export default Navbar;
